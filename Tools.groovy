0accf32ce0ebdb3469a9b8bdaf76db9e288975aa
Shubham Namdeo <shubham.namdeo@gmail.com>
2018-03-27 23:16:58 +0530

    Update Tools.groovy

--- Tools.groovy
+++ Tools.groovy
@@ -1,17 +1,19 @@
 public class Demo {
      String message;
+     private Object script
 
-     Demo(String message) {
+     Demo(String message, Object script) {
         this.message=message;
+        this.script=script;
      }
 
 
     public void print_data() {
-        println(message)
+        script.println(message)
     }
 }
 
-public Demo newSystemConfig(String m) {
-    return new Demo(m)
+public Demo newSystemConfig(String m, Object script) {
+    return new Demo(m,script)
 }
 return this
